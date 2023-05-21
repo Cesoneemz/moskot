b87e000426c967558ab83125c5cd5ab3530f2cb5
RG1ee <niwows@yandex.ru>
2023-05-21 20:18:20 +0300

    add endpoint for sign up to internship

--- internships/views.py
+++ internships/views.py
@@ -1,5 +1,11 @@
-from rest_framework import viewsets
 from django_filters import rest_framework as filters
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from user_auth.models import UserAccount
 
 from .serializers import InternShipSerializer
 from .models import InternShip
@@ -11,3 +17,12 @@ class InternShipViewSet(viewsets.ModelViewSet):
     serializer_class = InternShipSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = InternshipFilterSet
+
+    @action(methods=["GET"], detail=True)
+    def sign_up(self, request: Request, *args, **kwargs):
+        user = request.user
+        internship = get_object_or_404(UserAccount, pk=user.pk)
+        if internship.condidates.contains(user):
+            return Response("Вы уже подали заявку на эту стажировку!")
+        internship.condidates.add(user)
+        return Response("Вы успешно подали заявку на стажировку!")
