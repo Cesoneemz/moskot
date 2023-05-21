18bc3a176e9dfda377ec1e66281e6d19c56e711c
RG1ee <niwows@yandex.ru>
2023-05-21 21:36:07 +0300

    create auto documentation

--- lct_16_rest/urls.py
+++ lct_16_rest/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from internships.views import InternShipViewSet
 
@@ -14,5 +15,7 @@ urlpatterns = [
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("user/", include("user_auth.urls")),
-    path("api/v1/", include(router.urls)),
+    path("api/", include(router.urls)),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
 ]
